e1180b9b705495be4c8ed7aa6fae4ad6e8a081ff
808vita <its808vita@gmail.com>
2024-09-20 15:55:06 +0530

    import maps uber h3 project - components |utils | map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import dynamic from "next/dynamic";
+// import MapComponent from "@/components/MapComponent";
+const LazyMap = dynamic(() => import("@/components/MapComponent"), {
+  ssr: false,
+});
+
 export default function Home() {
-  return <div>oof</div>;
+  return (
+    <main className="leaflet-container">
+      {/* { <MapComponent />} */}
+      <LazyMap />
+    </main>
+  );
 }
