ad63b867fc252167922772c80000c0212f8b55c6
808vita <its808vita@gmail.com>
2024-09-20 19:09:39 +0530

    mapviz starters

---
+++ src/components/modularMapComponents/MapVizController.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+import {
+  Marker,
+  Popup,
+  Tooltip,
+  useMap,
+  useMapEvents,
+  Polygon,
+} from "react-leaflet";
+
+
+import { Icon } from "leaflet";
+import "leaflet/dist/leaflet.css";
+import { markerPath } from "@/utils/leafletConfig";
+
+const MapVizController = (props: any) => {
+  return (
+    <div>MapVizController</div>
+  )
+}
+
+export default MapVizController
